1bc6de8ac10df569bf32934ad7a52feafdd07109
rugvedpadhye <rugvedpadhye485@gmail.com>
2026-07-28 17:44:54 +0530

    Modified _0001_two_sum

--- _0001_Two_Sum.py
+++ _0001_Two_Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
         for i in range(n - 1):
             for j in range(i + 1, n):
